fe91567105823409871572098a861875ccc68493
shrimansoft <shrimansoft@hotmail.com>
2022-06-24 15:00:43 +0530

    yes

--- assets/contributing.html.d5573dc2.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./app.42d2e270.js";const r={};function n(o,_){return t(),c("div")}var i=e(r,[["render",n],["__file","contributing.html.vue"]]);export{i as default};
--- docs/assets/javascript/copyToClicpboard.js
+++
@@ -1,9 +0,0 @@
-    function CopyToClipboard(pass,link) {
-    var input = document.createElement('input');
-    input.value = pass;
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand("copy");
-    location.replace(link);
-    document.body.removeChild(input);
-    }
